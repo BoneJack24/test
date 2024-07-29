bfc3b34f00d4f871f1385a682da42c1e50165ce2
BoneJack24 <JABOI114@ulaval.ca>
2024-07-29 17:05:24 -0400

    Merge conflict

--- newFile.js
+++ newFile.js
@@ -2,11 +2,10 @@ Git status
 class Test 
 {
 	constructor() {}
-<<<<<<< HEAD
+
 	newAwesomeMethod (awesome, method) 
-=======
-	awesomeMethod (awesomeParamModified, methodModified) 
->>>>>>> e549e75ec51df74340b4285bc701d80e70e409f8
+
+	awesomeMethod (awesomeParamModified, methodModified)
 	{
 		console.log("Awesome!!");
 	}
